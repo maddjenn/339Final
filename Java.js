fae4f5ba3ba6ec4708ce38c31b616939c276b192
Mads <90150746+maddjenn@users.noreply.github.com>
2023-04-17 00:28:07 -0400

    Create Java.js

---
+++ Java.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" ></object>';
+  document.getElementById("nav").innerHTML = '<object type="text/html" data="nav.html" ></object>';
+  document.getElementById("footer").innerHTML = '<object type="text/html" data="footer.html" ></object>';
+
+  // add active class to current page in nav
+  var current_url = window.location.pathname;
+  var nav_links = document.getElementsByTagName("a");
+  for (var i = 0; i < nav_links.length; i++) {
+    if (nav_links[i].href == current_url) {
+      nav_links[i].classList.add("active");
+    }
+  }
+};
